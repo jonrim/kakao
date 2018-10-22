a253fda3d492192e8f21ee6b0a6552d111f26e99
Jonathan Rim <jonrim@umich.edu>
2018-10-22 19:00:12 -0400

    refactored chat messages, need to get a more reliable scroll change (scroll to bottom of the chat messages on mount

---
+++ src/components/chatroom/chatroomMessages/index.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+
+import './index.scss'
+
+export default class ChatroomMessages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.displayTimeForThisMessage = this.displayTimeForThisMessage.bind(this);
+  }
+
+  displayTimeForThisMessage(i) {
+    const { chatHistory } = this.props;
+    return (i + 1 === chatHistory.length || i + 1 < chatHistory.length &&
+          (chatHistory[i].date.diff(chatHistory[i + 1].date, 'minutes') ||
+          chatHistory[i].friend !== chatHistory[i + 1].friend));
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      let chatMessagesDiv = document.getElementsByClassName('chatroom-messages')[0];
+      chatMessagesDiv.scrollTop = chatMessagesDiv.scrollHeight;
+    }, 2000)
+  }
+
+  render() {
+    const { chatHistory, chatroom } = this.props;
+    const { displayTimeForThisMessage } = this;
+    return (
+      <div className='chatroom-messages'>
+        {
+          chatHistory.map((message, i) => (
+            <div 
+              className={(message.friend ? 'her-message' : 'my-message') + ' message'} 
+              key={'message' + i}
+              style={{
+                /*
+                  separate (more) texts with different time stamps
+                */
+                marginBottom: displayTimeForThisMessage(i) ? '8px' : '0'
+              }}
+            >
+              {
+                message.firstMessageOfDay && (
+                  <p className='date' style={{margin: '15px 0'}}>
+                    {
+                      message.date.format('dddd, MMMM D, YYYY')
+                    }
+                  </p>
+                )
+              }
+              {/*
+                flex container used for its child (right sibling) to extend its width to the remaining width of
+                the container. this is so the actual text bubble can utilize most of the width. e.g. short
+                texts can now be displayed on one line instead of awkwardly being put on 3 lines.
+              */}
+              <div style={{display: 'flex'}}>
+                {
+                  message.friend &&
+                  <div style={{width: '40px', display: 'inline-block'}}>
+                  {
+                    message.firstMessageOfMinute &&
+                    <img className='chatroom-photo' src={chatroom.photo}/>
+                  }
+                  </div>
+                }
+                <div 
+                  className='clearfix' 
+                  style={{
+                    /* 
+                      if it's a friend's message, or if it's one of my messages that doesn't display the time,
+                      make this clearfix container an inline-block. otherwise use flex for my own messages that show time
+                      so that I can properly position the time to be at the bottom right corner of it's container,
+                      just to the left of the message 
+                     */
+                    display: !displayTimeForThisMessage(i) || chatHistory[i].friend ? 'inline-block' : 'flex',
+                    verticalAlign: 'top',
+                    marginLeft: '3px',
+                    position: 'relative',
+                    flex: 1
+                  }}>
+                  {
+                    message.friend && message.firstMessageOfMinute &&
+                    <p style={{margin: '0', padding: '0 10px'}}>{chatroom.name}</p>
+                  }
+                  {
+                    displayTimeForThisMessage(i) && !chatHistory[i].friend &&
+                    <div className='date-container'>
+                      <span>{ message.date.format('h:mm A') }</span>
+                    </div>
+                  }
+                  <p className='text'>{ message.text }</p>
+                  {
+                    displayTimeForThisMessage(i) && chatHistory[i].friend &&
+                    <div className='date-container'>
+                      <span>{ message.date.format('h:mm A') }</span>
+                    </div>
+                  }
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+}
--- src/components/chatroom/index.js
+++ src/components/chatroom/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Input, Form, TextArea, Button } from 'semantic-ui-react'
 import moment from 'moment'
+import ChatroomMessages from './chatroomMessages'
 
 import './index.scss'
 
@@ -84,7 +85,6 @@ export default class Chatroom extends Component {
     };
     this.showSearch = this.showSearch.bind(this);
     this.showMore = this.showMore.bind(this);
-    this.displayTimeForThisMessage = this.displayTimeForThisMessage.bind(this);
   }
 
   showSearch() {
@@ -98,13 +98,6 @@ export default class Chatroom extends Component {
 
   }
 
-  displayTimeForThisMessage(i) {
-    const { chatHistory } = this.state;
-    return (i + 1 === chatHistory.length || i + 1 < chatHistory.length &&
-          (chatHistory[i].date.diff(chatHistory[i + 1].date, 'minutes') ||
-          chatHistory[i].friend !== chatHistory[i + 1].friend));
-  }
-
   componentDidMount() {
     document.getElementById('chatroom-type-input').focus();
   }
@@ -112,7 +105,7 @@ export default class Chatroom extends Component {
   render() {
     const { chatroom, changeFriendState, mobileWindow } = this.props;
     const { search, chatHistory } = this.state;
-    const { showSearch, showMore, displayTimeForThisMessage } = this;
+    const { showSearch, showMore } = this;
     return (
       <div className='chatroom'>
         <div className='chatroom-nav'>
@@ -144,81 +137,10 @@ export default class Chatroom extends Component {
             />
           </div>
         }
-        <div className='chatroom-messages'>
-          {
-            chatHistory.map((message, i) => (
-              <div 
-                className={(message.friend ? 'her-message' : 'my-message') + ' message'} 
-                key={'message' + i}
-                style={{
-                  /*
-                    separate (more) texts with different time stamps
-                  */
-                  marginBottom: displayTimeForThisMessage(i) ? '8px' : '0'
-                }}
-              >
-                {
-                  message.firstMessageOfDay && (
-                    <p className='date' style={{margin: '15px 0'}}>
-                      {
-                        message.date.format('dddd, MMMM D, YYYY')
-                      }
-                    </p>
-                  )
-                }
-                {/*
-                  flex container used for its child (right sibling) to extend its width to the remaining width of
-                  the container. this is so the actual text bubble can utilize most of the width. e.g. short
-                  texts can now be displayed on one line instead of awkwardly being put on 3 lines.
-                */}
-                <div style={{display: 'flex'}}>
-                  {
-                    message.friend &&
-                    <div style={{width: '40px', display: 'inline-block'}}>
-                    {
-                      message.firstMessageOfMinute &&
-                      <img className='chatroom-photo' src={chatroom.photo}/>
-                    }
-                    </div>
-                  }
-                  <div 
-                    className='clearfix' 
-                    style={{
-                      /* 
-                        if it's a friend's message, or if it's one of my messages that doesn't display the time,
-                        make this clearfix container an inline-block. otherwise use flex for my own messages that show time
-                        so that I can properly position the time to be at the bottom right corner of it's container,
-                        just to the left of the message 
-                       */
-                      display: !displayTimeForThisMessage(i) || chatHistory[i].friend ? 'inline-block' : 'flex',
-                      verticalAlign: 'top',
-                      marginLeft: '3px',
-                      position: 'relative',
-                      flex: 1
-                    }}>
-                    {
-                      message.friend && message.firstMessageOfMinute &&
-                      <p style={{margin: '0', padding: '0 10px'}}>{chatroom.name}</p>
-                    }
-                    {
-                      displayTimeForThisMessage(i) && !chatHistory[i].friend &&
-                      <div className='date-container'>
-                        <span>{ message.date.format('h:mm A') }</span>
-                      </div>
-                    }
-                    <p className='text'>{ message.text }</p>
-                    {
-                      displayTimeForThisMessage(i) && chatHistory[i].friend &&
-                      <div className='date-container'>
-                        <span>{ message.date.format('h:mm A') }</span>
-                      </div>
-                    }
-                  </div>
-                </div>
-              </div>
-            ))
-          }
-        </div>
+        <ChatroomMessages
+          chatHistory={chatHistory}
+          chatroom={chatroom}
+        />
         <div className='chatroom-type'>
           <Form>
             <TextArea autoHeight rows='2' id='chatroom-type-input'/>
